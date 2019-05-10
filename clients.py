310cf82e6aa4bc451b92f5fb1494fa0aba91d648
Ryan Barner <emailryanbarner@gmail.com>
2019-05-10 08:55:47 -0700

    Added spacing for better readability; made quotation marks consistent; updated class method to return erorr string instead of exception as FordBikes only has one region; changed format of fstrings for easier updating later; made homepage method verby in server; enabled running at commandline

--- clients.py
+++ clients.py
@@ -1,26 +1,53 @@
 import requests
 
 def get_all():
-    return [Jump()]
+    '''Return list of instantiated bike objects.'''
+    return [Jump(), FordBike()]
 
 class BaseBikeClass:
+   '''Base class for inheritance by bike provider.'''
    NAME = ''
    BASE_URL = ''
    METADATA = {}
 
    def all_bikes():
+       '''To ensure all_bikes method from base bike class is not inherited.'''
        raise NotImplementedError
 
 class Jump(BaseBikeClass):
+   '''Jump bike class inheriting BaseBikeClass attributes and methods.'''
    def __init__(self):
       self.NAME = 'jumpbikes'
       self.BASE_URL = 'jumpbikes.com/opendata'
       self.METADATA = {}
 
    def all_bikes(self, region):
-      if region not in ['atx', 'nyc', 'chi', 'sc', 'dc', 'san', 'sac', 'pvd', 'mia']:
-        raise Exception("There are no bikes in {0}".format(region))
-      url = "https://{0}.{1}/free_bike_status.json".format(region, self.BASE_URL)
+      if region not in ['atx', 'nyc', 'chi', 'sc', 'dc', 
+                        'san', 'sac', 'pvd', 'mia']:
+        return f'There are no bikes in {region.upper()}'
+        
+      url = f'https://{region}.{self.BASE_URL}/free_bike_status.json'
       r = requests.get(url)
       data = r.json()
+
       return data
+
+class FordBike(BaseBikeClass):
+   '''Ford bike class inheriting BaseBikeClass attributes and methods.'''
+   def __init__(self):
+      self.NAME = 'fordbike'
+      self.BASE_URL = 'gbfs.fordgobike.com/gbfs/en/station_status.json'
+      self.METADATA = {}
+
+   def all_bikes(self, region):
+      region = region.lower()
+      # FordBikes is BayArea exclusive (Berkeley, San Francisco, San Jose, etc.)
+      if region not in ['san']: 
+        return f'There are no bikes in {region.upper()}'
+
+      url = f'https://{self.BASE_URL}'
+      r = requests.get(url)
+      data = r.json()
+
+      return data
+
--- mobility.py
+++ mobility.py
@@ -4,15 +4,24 @@ import clients
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def greet_user():
    return "Ciao!"
 
 @app.route("/<region>/all")
-def all_bikes(region):
+def all_bike_data(region):
    try:
       res = {}
       for client in clients.get_all():
          res[client.NAME] = client.all_bikes(region)
+         
       return jsonify(res)
    except Exception as e:
       return jsonify({"error":str(e)})
+
+
+################################################################################
+# For running app at command line.
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0")
